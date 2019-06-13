ed517f4e4b38c3380ebe452bcb3574039bf163a5
Pdom <ahb0327@naver.com>
2019-06-13 16:11:22 +0900

    Change to es2015 project
    used generator-express-es6

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import Debug from 'debug';
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+// import favicon from 'serve-favicon';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import index from './routes/index';
 
-var app = express();
-
-// view engine setup
+const app = express();
+const debug = Debug('seoru:app');
 app.set('views', path.join(__dirname, 'views'));
+// view engine setup
 app.set('view engine', 'ejs');
-
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+/* eslint no-unused-vars: 0 */
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+// Handle uncaughtException
+process.on('uncaughtException', (err) => {
+  debug('Caught exception: %j', err);
+  process.exit(1);
+});
+
+export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+/* GET index page. */
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
-module.exports = router;
+export default router;
