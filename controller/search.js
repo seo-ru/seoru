dbb3fc4e4dcbe54f4721097904cf7525d39e9b41
Pdom <ahb0327@naver.com>
2019-06-14 11:49:04 +0900

    Update search.js

--- controller/search.js
+++ controller/search.js
@@ -1,7 +1,21 @@
+import sequelize from 'sequelize';
+
 import models from './../models';
 
+const Op = sequelize.Op;
+
 function getBooks(req, res) {
-  models.books.findAll({ limit: 10 }).then(books => res.json(books));
+  models.books.findAll({
+    where: {
+      title: {
+        [Op.like]: `%${req.query.query}%`
+      }
+    },
+    limit: 10
+  }).then(books => res.render('search', {
+    books
+  })
+  );
 }
 
 function getBook(req, res) {
